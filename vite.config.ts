dfbbf4b080ae05068e3ad60bcd59e2cba5fcf5ea
GeLoader <zinfotech1996@gmail.com>
2026-03-18 17:24:30 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/land-link-dream/",
+  base: "/land-network/",
   server: {
     host: "::",
     port: 8080,
